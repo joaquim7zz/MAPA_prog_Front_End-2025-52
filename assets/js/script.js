3c1e9e18e9d92ec63d0296e315ae934453baff50
joaquim7zz <162745299+joaquim7zz@users.noreply.github.com>
2025-06-24 20:47:35 -0300

    Página "contato" e "mensagem" criadas, configuras com js e estilizada

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Função para obter o valor da URL
+function obterURL(nome) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(nome);
+}
+
+// Quando a página carregar, exibe a mensagem de agradecimento
+window.addEventListener('load', function() {
+    const nome = obterURL('nome');
+    
+    if (nome) {
+        // Mensagem de agredecimento
+        const mensagemAgradecimento = `Olá, ${nome}! Muito obrigado por testar o site até aqui!`;
+        
+        const exibeMensagem = document.querySelector('.mensagem');
+        
+        //exibe a mensagem na tela do cliente 
+        if (exibeMensagem) {
+            exibeMensagem.innerHTML = mensagemAgradecimento;
+            exibeMensagem.style.display = 'block'
+        }
+    }
+});
